6e202dcb88268f7c731304656e7e6293b492fa1a
Colin Finger <colin.finger@pmcs.de>
2021-12-12 19:01:18 +0100

    tailwind properly working (y)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    // mode: 'jit',
+    purge: ['./src/**/*.{html,ts}'],
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
-        colors: {
-            'blue': '#0082BB'
+        fontFamily: {
+            'opensans': 'Open Sans'
         },
         extend: {
-
+            boxShadow: {
+                'shadow': '0px 1px 5px 0px rgba(0, 0, 0, 0.12), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.2)'
+            },
+            colors: {
+                primary: {
+                    'normal': 'rgba(0, 130, 187, 1)',
+                    'hover': 'rgba(0, 130, 187, 0.92)',
+                    'focus': 'rgba(0, 130, 187, 0.76)',
+                    'active': 'rgba(0, 130, 187, 0.76)'
+                },
+                gray: {
+                    '500': '#e0e0e0'
+                }
+            }
         },
+    },
+    variants: {
+        extend: {},
     },
     plugins: [],
 };
